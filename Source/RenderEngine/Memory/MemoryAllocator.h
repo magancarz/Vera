5e3b5ec8ac7a778708146cd17f29aa2bc5d6edce
magancarz <grzmateusz6@gmail.com>
2024-05-26 12:34:05 +0200

    [Feature] added GLFW and gtest as submodules

--- Source/RenderEngine/Memory/MemoryAllocator.h
+++ Source/RenderEngine/Memory/MemoryAllocator.h
@@ -1,7 +1,7 @@
-#pragma once
-
-class MemoryAllocator
-{
-public:
-    std::unique_ptr<Buffer> allocateBuffer(uint32_t size, uint32_t memory_flags, uint32_t usage_flags);
-};
+//#pragma once
+//
+//class MemoryAllocator
+//{
+//public:
+//    std::unique_ptr<Buffer> allocateBuffer(uint32_t size, uint32_t memory_flags, uint32_t usage_flags);
+//};
