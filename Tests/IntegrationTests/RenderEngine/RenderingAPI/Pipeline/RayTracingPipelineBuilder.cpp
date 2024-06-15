182ed9e2fcdb93bed29145ae829c87a2aad3c422
magancarz <grzmateusz6@gmail.com>
2024-06-15 14:27:10 +0200

    [Fix] fixed tests and reorganized main cmake file

--- Tests/IntegrationTests/RenderEngine/RenderingAPI/Pipeline/RayTracingPipelineBuilder.cpp
+++ Tests/IntegrationTests/RenderEngine/RenderingAPI/Pipeline/RayTracingPipelineBuilder.cpp
@@ -9,14 +9,20 @@
 TEST(RayTracingPipelineBuilderTests, shouldBuildValidRayTracingPipeline)
 {
     // given
-    auto descriptor_set_layout = DescriptorSetLayoutBuilder(TestsEnvironment::vulkanHandler())
+    auto acceleration_structure_descriptor_set_layout = DescriptorSetLayoutBuilder(TestsEnvironment::vulkanHandler())
         .addBinding(0, VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR, VK_SHADER_STAGE_RAYGEN_BIT_KHR | VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR)
-        .addBinding(1, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, VK_SHADER_STAGE_RAYGEN_BIT_KHR)
-        .addBinding(2, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_RAYGEN_BIT_KHR | VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR)
-        .addBinding(3, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR | VK_SHADER_STAGE_ANY_HIT_BIT_KHR)
-        .addBinding(4, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR | VK_SHADER_STAGE_ANY_HIT_BIT_KHR)
-        .addBinding(5, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR | VK_SHADER_STAGE_ANY_HIT_BIT_KHR)
-        .addBinding(6, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR | VK_SHADER_STAGE_ANY_HIT_BIT_KHR)
+        .build();
+
+    auto ray_traced_image_descriptor_set_layout = DescriptorSetLayoutBuilder(TestsEnvironment::vulkanHandler())
+        .addBinding(0, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, VK_SHADER_STAGE_RAYGEN_BIT_KHR)
+        .build();
+
+    auto object_descriptions_descriptor_set_layout = DescriptorSetLayoutBuilder(TestsEnvironment::vulkanHandler())
+        .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_RAYGEN_BIT_KHR | VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR)
+        .addBinding(1, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR | VK_SHADER_STAGE_ANY_HIT_BIT_KHR)
+        .addBinding(2, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR | VK_SHADER_STAGE_ANY_HIT_BIT_KHR)
+        .addBinding(3, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR | VK_SHADER_STAGE_ANY_HIT_BIT_KHR)
+        .addBinding(4, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR | VK_SHADER_STAGE_ANY_HIT_BIT_KHR)
         .build();
 
     auto ray_tracing_pipeline_builder = RayTracingPipelineBuilder(TestsEnvironment::vulkanHandler(), TestsEnvironment::memoryAllocator())
@@ -24,7 +30,9 @@ TEST(RayTracingPipelineBuilderTests, shouldBuildValidRayTracingPipeline)
             .addMissStage(std::make_unique<ShaderModule>(TestsEnvironment::vulkanHandler(), "raytrace", VK_SHADER_STAGE_MISS_BIT_KHR))
             .addMissStage(std::make_unique<ShaderModule>(TestsEnvironment::vulkanHandler(), "raytrace_shadow", VK_SHADER_STAGE_MISS_BIT_KHR))
             .addDefaultOcclusionCheckShader(std::make_unique<ShaderModule>(TestsEnvironment::vulkanHandler(), "raytrace_occlusion", VK_SHADER_STAGE_ANY_HIT_BIT_KHR))
-            .addDescriptorSetLayout(descriptor_set_layout->getDescriptorSetLayout())
+            .addDescriptorSetLayout(acceleration_structure_descriptor_set_layout->getDescriptorSetLayout())
+            .addDescriptorSetLayout(ray_traced_image_descriptor_set_layout->getDescriptorSetLayout())
+            .addDescriptorSetLayout(object_descriptions_descriptor_set_layout->getDescriptorSetLayout())
             .setMaxRecursionDepth(2);
 
     ray_tracing_pipeline_builder.addMaterialShader(
